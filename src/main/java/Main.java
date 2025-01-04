bf8f39956eeae0225cbf97e4dc2de52179c8176a
Crow475 <116875872+Crow475@users.noreply.github.com>
2025-01-04 15:41:34 +0400

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public  static  void main(String[] args) {
+        System.out.println(test());
+    }
+
+    public static  String test() {
+        return ("Test string");
+    }
 }
